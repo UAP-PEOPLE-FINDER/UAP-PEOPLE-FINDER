18832ed78df29ecc1ea2f639de36b26231a36b76
shihab <19201051@uap-bd.edu>
2023-03-24 22:20:26 +0600

    added searching option

--- people_finder/main/views.py
+++ people_finder/main/views.py
@@ -254,3 +254,39 @@ def password_reset_profile_request(request):
         template_name="main/password/password_reset_profile.html",
         context={"password_reset_form": password_reset_form},
     )
+
+
+@login_required(login_url="main:login")
+def search_request(request):
+    search_form = SearchForm()
+    if request.method == "POST":
+        searchV = request.POST["search_text"]
+        searchV = searchV.strip()
+        if searchV:
+            searchValue = User.objects.filter(email__icontains=searchV)
+            if not (searchValue):
+                searchValue = Profile.objects.filter(first_name__icontains=searchV)
+            if not (searchValue):
+                searchValue = Profile.objects.filter(last_name__icontains=searchV)
+
+            tempObj = set()
+            for user in searchValue:
+                try:
+                    obj = Profile.objects.get(username=user)
+                    tempObj.add(obj)
+                except Exception as msg:
+                    print(msg)
+
+            table = SearchTable(tempObj)
+
+            return render(
+                request=request,
+                template_name="main/search.html",
+                context={"search_form": search_form, "table": table},
+            )
+
+    return render(
+        request=request,
+        template_name="main/search.html",
+        context={"search_form": search_form},
+    )
