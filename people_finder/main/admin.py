79d273cf3e51e9e73681148eda97252bf1ecd5ce
shihab <19201051@uap-bd.edu>
2023-03-24 22:04:53 +0600

    adding the profile viewer

--- people_finder/main/admin.py
+++ people_finder/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here
+def get_model_fields(model):
+    return [field.name for field in model._meta.get_fields()]
