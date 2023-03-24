37cdef0dd90f65634a14f623bac6c1c03d6f80df
shihab <19201051@uap-bd.edu>
2023-03-24 22:15:06 +0600

    added profile form

--- people_finder/main/form.py
+++ people_finder/main/form.py
@@ -3,6 +3,7 @@ from pyexpat import model
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile, ListofInterests, Interest
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -22,3 +23,28 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class ProfileForm(forms.Form):
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    display_picture = forms.ImageField()
+    interest_1 = forms.ModelChoiceField(
+        queryset=ListofInterests.objects.all(), required=False
+    )
+    interest_1_bio = forms.CharField(max_length=140, required=False)
+    interest_1_link = forms.CharField(max_length=100, required=False)
+
+    interest_2 = forms.ModelChoiceField(
+        queryset=ListofInterests.objects.all(), required=False
+    )
+    interest_2_bio = forms.CharField(max_length=140, required=False)
+    interest_2_link = forms.CharField(max_length=100, required=False)
+
+    interest_3 = forms.ModelChoiceField(
+        queryset=ListofInterests.objects.all(), required=False
+    )
+    interest_3_bio = forms.CharField(max_length=140, required=False)
+    interest_3_link = forms.CharField(max_length=100, required=False)
+
+    # interest1 = forms.
