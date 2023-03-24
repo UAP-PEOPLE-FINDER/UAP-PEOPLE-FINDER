f019cb4609fe39d688653d70ccf6d94fbe6b0c88
shihab <19201051@uap-bd.edu>
2023-03-24 22:06:28 +0600

    interest model

--- people_finder/main/admin.py
+++ people_finder/main/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from .models import ListofInterests, Profile, Interest
 
 # Register your models here
 def get_model_fields(model):
     return [field.name for field in model._meta.get_fields()]
+
+class ListofInterestsAdmin(admin.ModelAdmin):
+    fields = ["interest"]
